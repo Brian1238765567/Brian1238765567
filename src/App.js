e6e5a87527353832a060c4ee8e69aa71c8a83aae
Brian1238765567 <qwerty445245245245@gmail.com>
2021-09-22 09:32:37 -0400

    import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Amplify from 'aws-amplify-react';
 import DisplayPosts from './components/DisplayPosts'
 import CreatePost from './components/CreatePost';
 import { withAuthenticator } from 'aws-amplify-react'
 import { AmplifyAuthenticator, AmplifySignout} from '@aws-amplify/ui-react'
+import awsConfig from './aws-exports'
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
